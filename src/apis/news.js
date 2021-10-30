dc0ca33f654c92dac55e1d6453d76c859ac38589
suhas-kadu <kadusuhas72@gmail.com>
2021-10-30 10:09:28 +0530

    #fix: apikey

--- src/apis/news.js
+++ src/apis/news.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = `${process.env.REACT_APP_NEWS_API_KEY}`;
+const KEY = "36ddfae6e6474baebb50db8773f0bf3d";
 
 export default axios.create({
   baseURL: "https://newsapi.org/v2/everything",
